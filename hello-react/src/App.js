4c3cd79eda8b765ebc14d38be9b1d33a429f4e7b
JinBae Jeong <jinbaejeong@JinBaeui-MacBookPro.local>
2018-11-06 23:34:41 +0900

    Component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
-    const text = '당신은 어썸한가요?';
-    const condition = 13;
-    const style = {
-      backgroundColor : 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) + 50,
-      width: Math.round(Math.random() * 300) + 50,
-      WebkitTransition: 'all',
-      MozTransition: 'all',
-      msTransition: 'all'
-    }
     return (
-      <div className="my-div">
-        <h1>리액트 안녕!</h1>
-        <h2>{text}</h2>
-        {condition > 12 && '보여주세요'}
-        <div style={style}></div>
-      </div>
+      <MyComponent
+        name={"Raon"}
+        age={3}
+      />
     );
   }
 }
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
-    render(){
-        return(
+    //2번째 방식
+    static defaultProps = {
+        name : '기본 이름'
+    }
+    static propTypes = {
+        name : PropTypes.string,               // name props 타입을 문자열로 설정합니다.
+        age : PropTypes.number.isRequired      // 필수적으로 존재해야 하며, 숫자입니다.
+    }
+    state = {
+        number : 0
+    }
+    render() {
+        return (
             <div>
-                나의 새롭고 멋진 컴포넌트
+                <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
+                <p>저는 {this.props.age}살 입니다.</p>
+                <p>숫자 : {this.state.number}</p>
+                <button onClick = {() => {
+                    this.setState({
+                        number : this.state.number + 1
+                    })
+                }}>더하기</button>
+                <button onClick = {() => {
+                    this.setState({
+                        number : this.state.number - 1
+                    })
+                }}>빼기</button>
             </div>
-        )
+        );
     }
 }
+
+// 전통적 방식
+// MyComponent.defaultProps = {
+//     name : '기본 이름'
+// }
+// MyComponent.propTypes = {
+//     name : PropTypes.string
+// }
+
+export default MyComponent;
